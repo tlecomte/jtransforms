e19b0d164ac3ddf0cecb70bc4422236b5c9a0296
Timothée Lecomte <timothee.lecomte@pasteur.fr>
2012-03-06 09:37:29 +0100

    Actually use the parameter as the sleep time in the sleep method

--- src/edu/emory/mathcs/utils/ConcurrencyUtils.java
+++ src/edu/emory/mathcs/utils/ConcurrencyUtils.java
@@ -95,7 +95,7 @@ public class ConcurrencyUtils {
      */
     public static void sleep(long millis) {
         try {
-            Thread.sleep(5000);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
